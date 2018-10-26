923494b4dd366ce1fb753b652816fde047894a39
rparamo <rparamo11@gmail.com>
2018-10-26 01:45:34 -0500

    gifs are displayed on document

--- assets/comics.js
+++ assets/comics.js
@@ -28,5 +28,44 @@ $(document).ready(function() {
         });
     }
 
+// when user button clicked gifs will display
+    function display() {
+        var search = $(this).attr("data-search");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search  + "&api_key=fScMijy8O0JEXrlQYgi9zqjDQDhVTOlV&limit=12"; 
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            // console.log(response);
+            $("#gifs-show-here").empty();
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++){
+                var searchDiv = $("<div>");
+                searchDiv.addClass("searchDiv");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                var image = $("<img>");
+                
+                image.attr("src", results[i].images.fixed_height_still.url);
+                image.attr("data-still", results[i].images.fixed_height_still.url);
+                image.attr("data-animate", results[i].images.fixed_height.url);
+                image.attr("data-state", "still");
+                image.addClass("gif");
+
+                searchDiv.addClass("gif");
+                searchDiv.append(image);
+                searchDiv.append(p);
+
+                $("#gifs-show-here").prepend(searchDiv);
+            }
+        });
+    }
+
+    userButtons();
     addComics();
-})
+    display();
+});
